279910260d2669fbbd925a552f3141794fcbfd73
Zohidjon-Toshpulatov <44026027+Zohidjon-Toshpulatov@users.noreply.github.com>
2023-08-21 19:11:22 +0500

    git ignore create

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const timeSlotRouter = require(`${__dirname}/routes/timeSlotRoutes.js`);
 const cors = require('cors');
+const serverless = require('serverless-http')
 
 const app = express();
 app.use(express.json());
 
 const corsOptions = {
-    origin: '*', // Allow this origin
-    methods: '*', // Allow these methods
-    allowedHeaders: 'Content-Type' // Allow this header
-  };
-  app.use(cors(corsOptions));
+  origin: '*', // Allow this origin
+  methods: '*', // Allow these methods
+  allowedHeaders: 'Content-Type' // Allow this header
+};
+app.use(cors(corsOptions));
 
 app.use("/api/timeslots", timeSlotRouter);
 
 module.exports = app;
+module.exports.handler = serverless(app)
